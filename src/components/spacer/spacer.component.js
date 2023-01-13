494e4508b3725a5b20488d16948d7e88a000b415
Seunoke0310 <triumphantoke7@gmail.com>
2023-01-13 19:11:18 +0100

    optimized spacer component

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,45 +1,32 @@
-import React from "react";
 import styled from "styled-components/native";
 
-const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[1]};
-`;
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
-const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
-`;
+const positionVariant = {
+  top: 'marginTop',
+  left: 'marginLeft',
+  right: 'marginRight',
+  bottom: 'marginBottom',
+};
 
-const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
-`;
+const getVariant = (position, size) => {
+   const sizeIndex = sizeVariant[size];
+   const property = positionVariant[position];
+   const value = theme.space[sizeIndex];
+ 
+   return `${property}:${value}`;
+};
 
-const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
+export const Spacer = styled.View`y
+  ${({ position, size, theme }) => getVariant(position, size, theme)} 
 
-const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
+`; 
 
-const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
-export const Spacer = ({ variant }) => {
-  if (variant === "top.medium") {
-    return <TopMedium />;
-  }
-  if (variant === "top.large") {
-    return <TopLarge />;
-  }
-  if (variant === "left.small") {
-    return <LeftSmall />;
-  }
-  if (variant === "left.medium") {
-    return <LeftMedium />;
-  }
-  if (variant === "left.large") {
-    return <LeftLarge />;
-  }
-  return <TopSmall />;
+Spacer.defaultProps = {
+  position = 'top',
+  size = 'small',
 };
--- src/features/restuarants/components/restaurant-info-card.component.js
+++ src/features/restuarants/components/restaurant-info-card.component.js
@@ -80,10 +80,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <Spacer variant="left.large" />
-            {isOpenNoW && <SvgXml xml={open} width={20} height={20} />}
-            <Spacer variant="left.large" />
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            <Spacer position="left" size="large">
+              {isOpenNoW && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
